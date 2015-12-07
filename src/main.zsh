99f5d0cf1f02f99775720f1129438f8588c0cd7b
bchatard <brice@chatard.fr>
2015-12-07 08:45:21 +0000

    Version: 0.5: fix for #2 (compatibility with default sed command from OSX)

--- src/main.zsh
+++ src/main.zsh
@@ -1,4 +1,4 @@
-
+#!/bin/zsh
 ORIG_IFS=${IFS}
 IFS='#########################'
 
@@ -63,7 +63,7 @@ getProjectsPath()
 
     if [[ -n ${projectsPath} ]]; then
         addDebug "Raw Projects Path:\n${projectsPath}"
-        projectsPath=`echo ${projectsPath} | sed -e 's/ value="//g' -e 's/"/\\n/g' -e "s/[$]USER_HOME[$]/${escapedHome}/g"`
+        projectsPath=`echo ${projectsPath} | sed -e 's/ value="//g' -e 's/"/\\\n/g' -e "s/[$]USER_HOME[$]/${escapedHome}/g"`
         addDebug "Projects Path:\n${projectsPath}"
     fi
 
